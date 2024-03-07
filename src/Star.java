83ddc602a7d6f6cb86cd15ca98350a1949853ce9
LEEJH <retyu12@naver.com>
2024-03-07 12:57:47 +0900

    Merge branch 'Day2' of https://github.com/retyu1234/Java.git into Day2

--- src/Star.java
+++ src/Star.java
@@ -1,17 +1,47 @@
-
+import java.util.Scanner;
 public class Star {
 
 	public static void main(String[] args) {
+		Scanner sc;
+		sc = new Scanner(System.in);
 		
+		int tr = scn(sc);    //입력
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 5; j >= i; j--) { // 공백찍기
+			if(i<=5/2) {
+				if(i<j-5/2) {
+					System.out.println("*");
+					
+				}else
+					System.out.println("1");
+			}prt("*");
+			if(i>5/2) {
+				if(i>j-5/2) {
+				System.out.println("*");
+				
+			}else
+				System.out.println("1");
+			}
+			prtln(); // 줄바꾸기
+		}
+		}
+
 
 	}
 
+	public static void prtln() { // 줄바꾸기 함수
+		System.out.println();
+	}
+
+	public static String prt(String a) { // print 문자열함수
+		System.out.print(a);
+		return a;
+	}
+	public static int scn(Scanner sc){      //int 입력함수(별크기 입력용)
+		int num=sc.nextInt();
+		return num;
+		
+	}
+
 }
-System.out.println("NUM1 input :");
-num1 = sc.nextInt();
-
-System.out.println("oper input :");
-ch = sc.next().charAt(0);
-
-System.out.println("NUM2 input");
-num2 = sc.nextInt();
--- src/Test.java
+++ src/Test.java
@@ -10,8 +10,10 @@ public class Test {
 		String[] name = new String[n];
 		int[][] score = new int[n][4];
 		float[] avg = new float[n];
+		
+		String [] scrName = {"name","kor","eng","mat","total","Avg"};
 
-		Test.fo(name, n, n, sc, score, avg);
+		Test.fo(name, n, n, sc, score, avg,scrName);
 		Test.prt(name, n, n, score, avg);
 
 		/*
@@ -37,12 +39,12 @@ public class Test {
 		return num;
 	}
 
-	public static void fo(String[] name, int i, int j, Scanner sc, int score[][], float avg[]) {
+	public static void fo(String[] name, int i, int j, Scanner sc, int score[][], float avg[],String scoName[]) {
 		for (i = 0; i < name.length; i++) {
-			System.out.print("이름:");
+			System.out.print(scoName[0]+"입력:");
 			name[i] = sc.next();
 			for (j = 0; j < 3; j++) {
-				System.out.print("점수:");
+				System.out.print(scoName[j+1]+"입력:");
 				score[i][j] = sc.nextInt();
 
 				score[i][3] += score[i][j];
